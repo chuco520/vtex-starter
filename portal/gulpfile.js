0c0488d503e090a15573790b18684e0c49869237
marceloanderson <marceloanderson84@gmail.com>
2016-03-28 13:53:10 -0300

    alterações

--- portal/gulpfile.js
+++ portal/gulpfile.js
@@ -1,114 +1,114 @@
 /**
- *  VTEX Starter Speed
- *  
+ *  VTEX Starter
  */
 
-'use strict';
+"use strict";
 
-// Include gulp & tools we'll use
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
-var gutil = require('gulp-util');
-var plumber = require('gulp-plumber');
-var sass = require('gulp-ruby-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var minifycss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var connect	= require('gulp-connect');
-var packageJson = require('./package.json');
+var gulp		= require('gulp'),
+	gulpLoadPlugins = require('gulp-load-plugins')(),
+	gutil       = require('gulp-util'),
+	plumber     = require('gulp-plumber'),
+	imagemin	= require('gulp-imagemin'),
+	jshint      = require('gulp-jshint'),
+	uglify		= require('gulp-uglifyjs'),
+	sass        = require('gulp-ruby-sass'),
+	sourcemaps  = require('gulp-sourcemaps'),
+	minifycss   = require('gulp-minify-css'),
+	rename      = require('gulp-rename'),
+	connect	    = require('gulp-connect'),
+	changed		= require('gulp-changed'),
+	ignore		= require('gulp-ignore'),
+	// modrewrite  = require('connect-modrewrite'),
+	packageJson = require('./package.json');
 
 var environment = process.env.VTEX_HOST || 'vtexcommercestable';
 var url = packageJson.accountName && !gutil.env.no ? 'http://' + packageJson.accountName + '.vtexlocal.com.br/?debugcss=true&debugjs2=true' : '';
 
 // Lint JavaScript
-gulp.task('jshint', function () {
+gulp.task('lint', function () {
   return gulp.src('src/scripts/**/*.js')
     .pipe(connect.reload())
-    .pipe($.jshint())
-    .pipe($.jshint.reporter('jshint-stylish'))
-    .pipe($.jshint.reporter('fail'));
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
+    // .pipe(jshint.reporter('default'));
 });
 
 // Concatenate and minify JavaScript
 gulp.task('scripts', function () {
   var sources = ['./src/scripts/**/*.js'];
   return gulp.src(sources)
-    // .pipe($.concat('main.min.js'))
-    // .pipe($.uglify({preserveComments: 'some'}))
+    // .pipe(gulpLoadPlugins.concat('main.min.js'))
+    // .pipe(gulpLoadPlugins.uglify({preserveComments: 'some'}))
     // Output files
-    .pipe(gulp.dest('build/arquivos'))
-    .pipe($.size({title: 'scripts'}));
+    .pipe(gulp.dest('build/arquivos'));
 });
 
 // Optimize images
 gulp.task('images', function () {
   return gulp.src('src/images/**/*')
-    .pipe($.cache($.imagemin({
+    .pipe(gulpLoadPlugins.imagemin({
       progressive: true,
       interlaced: true
-    })))
-    .pipe(gulp.dest('build/arquivos'))
-    .pipe($.size({title: 'images'}));
+    }))
+    .pipe(gulp.dest('build/arquivos'));
 });
 
 gulp.task('sass', function () {
-  return sass('src/styles', {sourcemap: false, noCache: true, style:'expanded'})
+  return sass('src/styles/*.scss', {sourcemap: false, noCache: true, style:'expanded'})
     .on('error', function (err) {
         console.error('Error! ', err.message);
     })
     .pipe(sourcemaps.write())
-	// .pipe(gulp.dest(paths.dest))
+	.pipe(gulp.dest('build/arquivos'))
 	// .pipe(rename({suffix: '.min'}))
 	// .pipe(minifycss())
-  	.pipe(gulp.dest('build/arquivos'))
-  	.pipe($.size({title: 'sass'}))
+ //  	.pipe(gulp.dest('build/arquivos'))
 	.pipe(connect.reload());
 });
 
 gulp.task('css', function () {
   return gulp.src('src/styles/**/*.css')
 	.pipe(gulp.dest('build/arquivos'))
-	.pipe($.size({title: 'css'}))
 	.pipe(connect.reload());
 });
 
-
-gulp.task('connect', function () {
-	connect.server({
-		host: '*',
-		root: './build/',
-		port: 80,
-		livereload: true,
-		middleware: function (connect, opt) {
-			return [
-				require('connect-livereload')({
-					disableCompression: true
-				}),
-				require('connect-http-please')({
-					replaceHost: function (h) {
-						return h.replace('vtexlocal', environment);
-					}
-				}),
-				function(req, res, next) {
-					req.headers.host = req.headers.host.replace('vtexlocal', environment);
-					return next();
-				},
-				require('connect-tryfiles')('**', 'http://portal.' + environment + '.com.br:80', {
-					cwd: 'build/'
-				}),
-				url ? require('open')(url) : gutil.noop(),
-				function(err, req, res, next) {
-					var errString, _ref, _ref1;
-					errString = (_ref = (_ref1 = err.code) != null ? _ref1.red : void 0) != null ? _ref : err.toString().red;
-					return gutil.log(errString, req.url.yellow);
+gulp.task('connect', function(){
+  connect.server({
+  	host: '*',
+    root: './build',
+    port: 80,
+    livereload: true,
+    middleware: function (connect, opt) {
+    	return [
+    		require('connect-livereload')({
+				disableCompression: true
+			}),
+			require('connect-http-please')({
+				replaceHost: function (h) {
+					return h.replace('vtexlocal', environment);
 				}
-			];
-		}
-	});
+			}),
+			function(req, res, next) {
+				req.headers.host = req.headers.host.replace('vtexlocal', environment);
+				return next();
+			},
+			require('connect-tryfiles')('**', 'http://portal.' + environment + '.com.br:80', {
+				cwd: 'build/'
+			}),
+			// url ? require('open')(url) : gutil.noop(),
+			function(err, req, res, next) {
+				var errString, _ref, _ref1;
+				errString = (_ref = (_ref1 = err.code) != null ? _ref1.red : void 0) != null ? _ref : err.toString().red;
+				return gutil.log(errString, req.url.yellow);
+			}
+    	];
+    }
+  });
 });
 
 gulp.task('watch', function () {
-	gulp.watch(['src/scripts/**/*.js'], ['jshint', 'scripts']);
+	gulp.watch(['src/scripts/**/*.js'], ['lint', 'scripts']);
 	gulp.watch(['src/styles/**/*.scss'], ['sass']);
 	gulp.watch(['src/styles/**/*.css'], ['css']);
 	gulp.watch(['src/images/**/*'], ['images']);
